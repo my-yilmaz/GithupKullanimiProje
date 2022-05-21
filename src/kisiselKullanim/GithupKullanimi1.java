65cf2100056d5832a93154dff19dfec3c99214b0
Muhammed Yılmaz <myyilmaz046@gmail.com>
2022-05-21 16:16:19 +0300

    2.version

--- src/kisiselKullanim/GithupKullanimi1.java
+++ src/kisiselKullanim/GithupKullanimi1.java
@@ -4,5 +4,8 @@ public class GithupKullanimi1 {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
